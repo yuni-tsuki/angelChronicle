1c3b9b80ca920e58492773dc2f66cd06fe6df0a2
yuni-tsuki <huzukihayama@gmail.com>
2023-09-10 11:29:52 +0900

    機能追加とコード修正

--- angelchronicle.rb
+++ angelchronicle.rb
@@ -1,58 +1,46 @@
-class Gacha
-    def initialize
-        @angels = {:raphael => {:name => "ラファエル", :hierarchy => "熾天使", :hp => 230, :offence => 20},
-        :michael => {:name => "ミカエル", :hierarchy => "熾天使", :hp => 200, :offence => 25},
-        :gabriel => {:name => "ガブリエル", :hierarchy => "熾天使", :hp => 220, :offence => 20},
-        :uriel => {:name => "ウリエル", :hierarchy => "熾天使", :hp => 200, :offence => 20},
-        :cherub => {:name => "ケルビム", :hierarchy => "智天使", :hp => 180, :offence => 20},
-        :throne => {:name => "ソロネ", :hierarchy => "座天使", :hp => 150, :offence => 17},
-        :dominion => {:name => "ドミニオン", :hierarchy => "主天使", :hp => 120, :offence => 15},
-        :dynamis => {:name => "ダイナミス", :hierarchy => "力天使", :hp => 100, :offence => 13},
-        :exsousia => {:name => "エクスシーア", :hierarchy => "能天使", :hp => 90, :offence => 10},
-        :arkhai => {:name => "アルケー", :hierarchy => "権天使", :hp => 80, :offence => 9},
-        :archangel => {:name => "アークエンジェル", :hierarchy => "大天使", :hp => 70, :offence => 8},
-        :normalangel => {:name => "エンジェル", :hierarchy => "天使", :hp => 60, :offence => 6}}
+class EngelGacha
+    def initialize 
+        @engels = {
+            :raphael => {:name => "ラファエル", :hierarchy => "熾天使", :hp => 230, :offence => 20},
+            :michael => {:name => "ミカエル", :hierarchy => "熾天使", :hp => 200, :offence => 25},
+            :gabriel => {:name => "ガブリエル", :hierarchy => "熾天使", :hp => 220, :offence => 20},
+            :uriel => {:name => "ウリエル", :hierarchy => "熾天使", :hp => 200, :offence => 20},
+            :cherub => {:name => "ケルビム", :hierarchy => "智天使", :hp => 180, :offence => 20},
+            :throne => {:name => "ソロネ", :hierarchy => "座天使", :hp => 150, :offence => 17},
+            :dominion => {:name => "ドミニオン", :hierarchy => "主天使", :hp => 120, :offence => 15},
+            :dynamis => {:name => "ダイナミス", :hierarchy => "力天使", :hp => 100, :offence => 13},
+            :exsousia => {:name => "エクスシーア", :hierarchy => "能天使", :hp => 90, :offence => 10},
+            :arkhai => {:name => "アルケー", :hierarchy => "権天使", :hp => 80, :offence => 9},
+            :archangel => {:name => "アークエンジェル", :hierarchy => "大天使", :hp => 70, :offence => 8},
+            :normalangel => {:name => "エンジェル", :hierarchy => "天使", :hp => 60, :offence => 6}
+        }
     end
 
-    def pickACard(n)
-        r = Random.new
-        probability = r.rand(n)
+    def pick_a_card(n)
+        probability = rand(n)
         case probability
-        when 0..1
-            return @angels[:raphael]
-        when 2..3
-            return @angels[:michael]
-        when 4..5
-            return @angels[:gabriel]
-        when 6..7
-            return @angels[:uriel]
-        when 8..12
-            return @angels[:cherub]
-        when 13..20
-            return @angels[:throne]
-        when 21..30
-            return @angels[:dominion]
-        when 31..45
-            return @angels[:dynamis]
-        when 46..55
-            return @angels[:exsousia]
-        when 56..65
-            return @angels[:arkhai]
-        when 66..80
-            return @angels[:archangel]
-        when 81..99
-            return @angels[:normalangel]
+        when 0..1 then @engels[:raphael]
+        when 2..3 then @engels[:michael]
+        when 4..5 then @engels[:gabriel]
+        when 6..7 then @engels[:uriel]
+        when 8..12 then @engels[:cherub]
+        when 13..20 then @engels[:throne]
+        when 21..30 then @engels[:dominion]
+        when 31..45 then @engels[:dynamis]
+        when 46..55 then @engels[:exsousia]
+        when 56..65 then @engels[:arkhai]
+        when 66..80 then @engels[:archangel]
+        else @engels[:normalangel]
         end
     end
 
-    def fiveGacha
-        deck = Array.new
+    def five_gacha
+        deck = Array.new #空のデッキを宣言
         puts "5連ガチャを開始します。"
-        for i in 1..5 do
-            puts "―――#{i}枚目―――"
-            deck.push(self.pickACard(99)) if i != 5
-            deck.push(self.pickACard(20)) if i == 5
-            print "#{deck[i - 1][:hierarchy]}#{deck[i - 1][:name]}を引いた！:"
+        5.times do |i|
+            puts "―――#{i + 1}枚目―――"
+            deck.push(self.pick_a_card(i == 4 ? 20 : 99)) #5枚目のエンジェルはソロネ以上確定
+            print "#{deck.last[:hierarchy]}#{deck.last[:name]}を引いた！:" #42行目で引いたエンジェルの階級と名前を表示
             gets
         end
         return deck
@@ -64,33 +52,46 @@ class Battle
         @deck = deck
     end
 
-    def playersAttack(i,enemysHP)
-        angelsOffence = @deck[i][:offence]
-        puts "#{@deck[i][:name]}は#{angelsOffence}のダメージを与えた！"
-        return enemysHP - angelsOffence
+    def players_attack(i,enemys_hp)
+        engels_offence = @deck[i][:offence]
+        puts "#{@deck[i][:name]}は#{engels_offence}のダメージを与えた！"
+        return enemys_hp - engels_offence
     end
 
-    def battleScene
-        enemysHP = 80
-        angelsSumHP = 0
-
-        for i in 0..4 do
-            angelsSumHP += @deck[i][:hp]
-        end
+    def battle_scene
+        enemys_hp = 300
+        engels_sum_hp = @deck[0..4].sum { |engel| engel[:hp] }
 
         puts "―――戦闘開始―――"
-        print "敵のHPは#{enemysHP}です。:"
+        print "エンジェルのHPの合計は#{engels_sum_hp}、敵のHPは#{enemys_hp}です。:"
         gets
-        puts "プレイヤーの攻撃！"
 
-        for i in 0..4 do
-            enemysHP = playersAttack(i, enemysHP)
+        4.times do
+            puts "プレイヤーの攻撃！"
+
+            for i in 0..4 do
+                enemys_hp = players_attack(i, enemys_hp)
+            end
+
+            break if enemys_hp <= 0 #敵のHPが尽きると戦闘シーンを抜ける
+
+            print "敵の残りHPは#{enemys_hp}です。:"
+            gets
+
+            puts "敵の攻撃！"
+
+            enemys_attack = rand(50..90)
+            engels_sum_hp -= enemys_attack
+
+            puts "敵から#{enemys_attack}のダメージを食らった！"
+
+            break if engels_sum_hp <= 0 #エンジェルのHPが尽きると戦闘シーンを抜ける
+
+            print "エンジェルのHPは合計#{engels_sum_hp}です。:"
+            gets
         end
 
-        print "敵の残りHPは#{enemysHP}です。:"
-        gets
-
-        if enemysHP >= 0
+        if enemys_hp >= 0
             puts "あなたは敵を倒せませんでした…"
         else
             puts "敵を倒しました！勝利です！"
@@ -101,10 +102,10 @@ end
 print "エンジェルクロニクルをスタートします。Enterキーを押してください。:"
 gets
 
-gacha = Gacha.new
+gacha = EngelGacha.new
 
-deck = gacha.fiveGacha
+deck = gacha.five_gacha
 
 battle = Battle.new(deck)
 
-battle.battleScene
+battle.battle_scene
